6dfea3b4a5c3246a75475a51641a5caf3ce8cc79
lifeashaze <34096693+lifeashaze@users.noreply.github.com>
2024-03-12 17:04:16 +0530

    fix: mongo connection + dup assignments

--- src/events/messageCreate/itdsl.js
+++ src/events/messageCreate/itdsl.js
@@ -2,31 +2,37 @@ const { EmbedBuilder } = require("discord.js");
 const { MongoClient } = require("mongodb");
 const uri ="mongodb+srv://admin:FwhszxosD5x97RSk@hazikeen.kt06qvo.mongodb.net/";
 const mongoClient = new MongoClient(uri);
-const assignments = [];
+let assignments = [];
 
+async function fetchData(){
+  try {
+    await mongoClient.connect();
+      console.log("Connected to the MongoDB server");
+      const db = mongoClient.db("classroom");
+      const collection = db.collection("ITDSL");
+      const data = await collection.find({}).toArray();
+      assignments = []
+      
+    
+      data.forEach((document) => {
+        const assignment = {
+          title: document.title,
+          dueDate: document.due_date,
+        };
+        assignments.push(assignment);
+      });
+      console.log("Assignments Fetched!");
 
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+}
 
 
 module.exports = async (message, client) => {
-  try {
-    await mongoClient.connect();
-    console.log("Connected to the MongoDB server");
-    const db = mongoClient.db("classroom");
-    const collection = db.collection("ITDSL");
-    const data = await collection.find({}).toArray();
 
-    data.forEach((document) => {
-      const assignment = {
-        title: document.title,
-        dueDate: document.due_date,
-      };
-      assignments.push(assignment);
-      console.log(assignment);
-    });
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
   if (message.content === "=itdsl") {
+    await fetchData()
     const embed = new EmbedBuilder()
       .setColor("#0099ff")
       .setTitle("ITDSL Assignments")
